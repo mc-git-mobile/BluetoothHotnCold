b9c5284be0f3f34c111858149fd1d9a505640c99
chris <cjohn268@msudenver.edu>
2019-05-03 16:26:48 -0600

    still adding more stuff rom flash beep

--- app/src/main/java/com/example/hotncold/MainActivity.kt
+++ app/src/main/java/com/example/hotncold/MainActivity.kt
@@ -1,50 +1,32 @@
 package com.example.hotncold
 
-import android.app.Activity
 import android.graphics.Color
-import android.graphics.Color.rgb
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import android.support.v7.app.AlertDialog
-import android.widget.SeekBar
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent;
+import android.content.IntentFilter
 import android.os.Build
 import android.support.annotation.RequiresApi
-import android.util.Log;
+import android.util.Log
+import android.view.View
+import android.widget.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import java.nio.file.Files.size
-import java.util.*
 import kotlin.collections.ArrayList as ArrayList1
-import android.content.IntentFilter
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import org.jetbrains.anko.toast
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val LOG_TAG: String? = null // Just for logging purposes. Could be anything. Set to app_name
-    private val REQUEST_ENABLE_BT = 99 // Any positive integer should work.
-    private var mBluetoothAdapter: BluetoothAdapter? = null
-
-    private var m_bluetooth_adapter:BluetoothAdapter? = null
-    private lateinit var m_paired_devices: Set<BluetoothDevice>
-    private val REQUEST_ENABLE_BLUETOOTH = 1
-
-    companion object {
-        val EXTRA_SSRI:String = "RSSI"
-        val EXTRA_ADRESS: String = "Device_address"
-    }
 
 
     var pings = 0
@@ -56,25 +38,38 @@ class MainActivity : AppCompatActivity() {
     //this array is for the merged one
     var color3 = intArrayOf(0, 0, 0)
 
+    private lateinit var m_paired_devices: Set<BluetoothDevice>
+
+    var show_list = false
+
+    var device_list = arrayListOf<String>("")
+    private var m_bluetooth_adapter: BluetoothAdapter? = null //holds the Bluetooth Adapter
+    private var arrayAdapter: ArrayAdapter<String>? = null // adapter for list view if needed
+    private var listView: ListView? = null // list view for action bar to show data if needed
+
+
+
+
+
+    private val mReceiver = object : BroadcastReceiver() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onReceive(context: Context, intent: Intent) {
+            handleBTDevice(intent)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        listView = findViewById(R.id.device_list)
+        arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, device_list)
+        listView?.adapter = arrayAdapter
 
-        m_bluetooth_adapter = BluetoothAdapter.getDefaultAdapter()
-        if (m_bluetooth_adapter == null) {
-            toast ("this device doesnt support bluetooth")
-            return
-        }
-        if(!m_bluetooth_adapter!!.isEnabled) {
-            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
-        }
-        //action_scan
-        refresh.setOnClickListener{ pairedDeviceList()}
+
 
 
 
@@ -127,112 +122,32 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-
-
-    /*private fun enableBluetoothOnDevice() {
-        if (mBluetoothAdapter == null) {
-            Log.e(LOG_TAG, "This device does not have a bluetooth adapter")
-            finish()
-            // If the android device does not have bluetooth, just return and get out.
-            // There's nothing the app can do in this case. Closing app.
-        }
-
-        // Check to see if bluetooth is enabled. Prompt to enable it
-        if (mBluetoothAdapter!!.isEnabled()) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == REQUEST_ENABLE_BT) {
-            if (resultCode == 0) {
-                // If the resultCode is 0, the user selected "No" when prompt to
-                // allow the app to enable bluetooth.
-                // You may want to display a dialog explaining what would happen if
-                // the user doesn't enable bluetooth.
-                Toast.makeText(this, "The user decided to deny bluetooth access", Toast.LENGTH_LONG).show()
-            } else
-                Log.i(LOG_TAG, "User allowed bluetooth access!")
-        }
-    }
-
-    private fun displayListOfFoundDevices() {
-        var arrayOfFoundBTDevices = ArrayList<BluetoothObject>()
-
-        // start looking for bluetooth devices
-        mBluetoothAdapter!!.startDiscovery()
-
-        // Discover new devices
-        // Create a BroadcastReceiver for ACTION_FOUND
-        val mReceiver = object : BroadcastReceiver() {
-
-            @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
-            override fun onReceive(context: Context, intent: Intent) {
-                val action = intent.action
-                // When discovery finds a device
-                if (BluetoothDevice.ACTION_FOUND == action) {
-                    // Get the bluetoothDevice object from the Intent
-                    val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
-
-                    // Get the "RSSI" to get the signal strength as integer,
-                    // but should be displayed in "dBm" units
-                    val rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, java.lang.Short.MIN_VALUE).toInt()
-
-                    // Create the device object and add it to the arrayList of devices
-                    val bluetoothObject = BluetoothObject()
-                    bluetoothObject.setBluetooth_name(device.name)
-                    bluetoothObject.setBluetooth_address(device.address)
-                    bluetoothObject.setBluetooth_state(device.bondState)
-                    bluetoothObject.setBluetooth_type(device.type)    // requires API 18 or higher
-                    bluetoothObject.setBluetooth_uuids(device.uuids)
-                    bluetoothObject.setBluetooth_rssi(rssi)
-
-                    arrayOfFoundBTDevices.add(bluetoothObject)
-
-                    // 1. Pass context and data to the custom adapter
-                    val adapter = FoundBTDevicesAdapter(applicationContext, arrayOfFoundBTDevices)
-
-                    // 2. setListAdapter
-                    setListAdapter(adapter)
+    private fun handleBTDevice(intent: Intent) {
+        val action = intent.action
+        // When discovery finds a device
+        if (BluetoothDevice.ACTION_FOUND == action) {
+            // Get the BluetoothDevice object from the Intent
+            val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+            val deviceName  =
+                if (device.name != null) {
+                    device.name.toString()
+                } else {
+                    "--no name--"
                 }
-            }
+
+            device_list.add("$deviceName, $device \n")
+            /*m_bluetooth_adapter!!.cancelDiscovery()
+            client = ConnectThread(device)  //FIX** remember and reconnect if interrupted?
+            client?.start()*/
+
         }
-        // Register the BroadcastReceiver
+    }
+
+    private fun setupDiscovery() {
         val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
         registerReceiver(mReceiver, filter)
+        m_bluetooth_adapter!!.startDiscovery()
     }
-
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
-    private fun getArrayOfAlreadyPairedBluetoothDevices(): ArrayList<BluetoothObject>? {
-        var arrayOfAlreadyPairedBTDevices: ArrayList<BluetoothObject>? = null
-
-        // Query paired devices
-        val pairedDevices = mBluetoothAdapter!!.getBondedDevices()
-        // If there are any paired devices
-        if (pairedDevices.size > 0) {
-            arrayOfAlreadyPairedBTDevices = ArrayList<BluetoothObject>()
-
-            // Loop through paired devices
-            for (device in pairedDevices) {
-                // Create the device object and add it to the arrayList of devices
-                val bluetoothObject = BluetoothObject()
-                bluetoothObject.setBluetooth_name(device.name)
-                bluetoothObject.setBluetooth_address(device.address)
-                bluetoothObject.setBluetooth_state(device.bondState)
-                bluetoothObject.setBluetooth_type(device.type)    // requires API 18 or higher
-                bluetoothObject.setBluetooth_uuids(device.uuids)
-
-                arrayOfAlreadyPairedBTDevices.add(bluetoothObject)
-            }
-        }
-
-        return arrayOfAlreadyPairedBTDevices
-    }*/
-
 
     fun mergeValues(){
         //0,50,100
@@ -246,7 +161,6 @@ class MainActivity : AppCompatActivity() {
         //val coast= Toast.makeText(applicationContext, "$percent1 , $percent2", Toast.LENGTH_LONG)
         // coast.show()
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -284,7 +198,37 @@ class MainActivity : AppCompatActivity() {
                 builder.show()
             true}
 
-            R.id.scan -> {
+            R.id.connect -> {
+                m_paired_devices = m_bluetooth_adapter!!.bondedDevices
+                val list : ArrayList<BluetoothDevice> = ArrayList()
+
+                if (!m_paired_devices.isEmpty()) {
+                    for (device:BluetoothDevice in m_paired_devices) {
+                        list.add(device)
+                        Log.i("device", ""+device)
+                    }
+                }
+                arrayAdapter?.notifyDataSetChanged()
+
+                var device_list:ListView = findViewById(R.id.device_list)
+                device_list.adapter = arrayAdapter
+
+                device_list.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                    val device: BluetoothDevice = list[position]
+                    val address: String = device.address
+
+                    //val intent = Intent(this, ControlActivity::class.java)
+                    //intent.putExtra(EXTRA_ADRESS, address)
+                    //startActivity(intent)
+                }
+
+
+                if (show_list == false) {
+                    device_list.visibility = View.VISIBLE
+                    refresh.visibility = View.VISIBLE
+                }
+
+
 
                 true
             }
